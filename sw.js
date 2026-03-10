74f9b73bff1817c42f43ff71048bace3bf4896b2
Kwonbong Kim <kwonbongkim@Kwonbongui-MacBookPro.local>
2026-03-11 02:39:44 +0900

    fix: OAuth 리다이렉트 URI 미스매치(400) 수정

--- sw.js
+++ sw.js
@@ -34,8 +34,7 @@ self.addEventListener('fetch', event => {
         url.includes('firebaseapp.com/__/auth/') ||
         url.includes('googleapis.com') ||
         url.includes('firebaseio.com') ||
-        url.includes('accounts.google.com') ||
-        url.includes('web.app')
+        url.includes('accounts.google.com')
     ) {
         return; // 서비스 워커가 개입하지 않음 (네트워크로 직접 이동)
     }
